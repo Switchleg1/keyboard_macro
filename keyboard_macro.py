221cd7e5fa6bf2151c70f9114ff3c041c2870021
Switchleg1 <89221799+Switchleg1@users.noreply.github.com>
2026-01-18 21:33:07 -0700

    using new keyboard hook method

--- keyboard_macro.py
+++ keyboard_macro.py
@@ -1,19 +1,22 @@
-import keyboard
 import json
 import time
 import yaml
+import keyboard
+
+from keyboard._keyboard_event import KEY_DOWN, KEY_UP
 
 #globals
+APP_VERSION         = "v0.2"
 CONFIG_FILENAME     = "config.yaml"
 DEFAULT_KEY_DELAY   = 0.05
 
-sequences = []
-key_delay = DEFAULT_KEY_DELAY
+key_map             = {}
+hot_keys            = []
+sequences           = []
+key_delay           = DEFAULT_KEY_DELAY
 
-record_name = ""
-record_filename = ""
-record_position = 0
-record_start = False
+record_hot_key      = None
+record_start        = False
 
 
 def read_configuration(filename):
@@ -47,19 +50,26 @@ def read_configuration(filename):
 
                 print(f"  Loading [{macro_dict['name']}] - [{macro_dict['filename']}]")
                 sequence = load_sequence(macro_dict['filename'])
-                if sequence is not None:
-                    sequences.append(sequence)
-
-                else:
+                if sequence is None:
                     break
 
+                # build hotkey
+                hot_key = {
+                    'name'      : macro_dict['name'],
+                    'filename'  : macro_dict['filename'],
+                    'sequence'  : sequence,
+                }
+
                 if 'play_hotkey' in macro_dict:
+                    hot_key['play_hotkey'] = macro_dict['play_hotkey'].split('+')
                     print(f"    assigning play_hotkey [{macro_dict['play_hotkey']}]")
-                    keyboard.add_hotkey(macro_dict['play_hotkey'], on_hotkey_play_trigger, args=[macro_positon, macro_dict['name']])
 
                 if 'record_hotkey' in macro_dict:
+                    hot_key['record_hotkey'] = macro_dict['record_hotkey'].split('+')
                     print(f"    assigning record_key [{macro_dict['record_hotkey']}]")
-                    keyboard.add_hotkey(macro_dict['record_hotkey'], on_hotkey_record_trigger, args=[macro_positon, macro_dict['name'], macro_dict['filename']])
+
+                # add hot_key to the list
+                hot_keys.append(hot_key)
 
                 macro_positon += 1
                 macro_string = f"macro_{macro_positon}"
@@ -70,18 +80,18 @@ def read_configuration(filename):
         print(f"Failed to load configuration file [{filename}] - {e}")
 
 
-def save_sequence(sequence_position, filename):
+def save_sequence(hot_key):
     try:
         # Serialize events to a list of JSON strings and save to a file
-        with open(filename, "w") as f:
-            json_events = [event.to_json() for event in sequences[sequence_position]]
+        with open(hot_key['filename'], "w") as f:
+            json_events = [event.to_json() for event in hot_key['sequence']]
             # Use json.dump for a structured JSON file with a list of events
             json.dump(json_events, f, indent=4)
 
-        print(f"Recording saved to {filename}")
+        print(f"Recording saved to {hot_key['filename']}")
 
     except Exception as e:
-        print(f"Failed to save {filename} - {e}")
+        print(f"Failed to save {hot_key['filename']} - {e}")
 
 
 def load_sequence(filename):
@@ -108,50 +118,79 @@ def load_sequence(filename):
         return None
 
 
-def on_hotkey_play_trigger(sequence_position, sequence_name):
-    print(f"Playing - {sequence_name}") #[{sequences[sequence_position]}]")
-    keyboard.play(sequences[sequence_position], speed_factor=1.0)
+def on_play_key(hot_key):
+    print(f"Playing - {hot_key['name']}") #[{hot_key['sequence']}]")
+    keyboard.play(hot_key['sequence'], speed_factor=1.0)
 
 
-def on_hotkey_record_trigger(sequence_position, sequence_name, sequence_filename):
-    global record_name
-    global record_filename
+def on_record_key(hot_key):
+    global record_hot_key
     global record_start
-    global record_position
 
     if record_start == False:
-        record_name = sequence_name
-        record_filename = sequence_filename
-        record_position = sequence_position
+        record_hot_key = hot_key
         record_start = True
+
+
+def check_hot_key(hot_key_string):
+    for c in hot_key_string: 
+        if c not in key_map or key_map[c] == False:
+            return False
+
+    return True
+
+
+def on_key_pressed(key):
+    key_map[key] = True
+
+    for hot_key in hot_keys:
+        if 'play_hotkey' in hot_key and check_hot_key(hot_key['play_hotkey']):
+            on_play_key(hot_key)
+
+        if 'record_hotkey' in hot_key and check_hot_key(hot_key['record_hotkey']):
+            on_record_key(hot_key)
+
+
+def on_key_released(key):
+    key_map[key] = False
+
+
+def on_key_event(event):
+    if event.event_type == KEY_DOWN:
+        on_key_pressed(event.name)
+
+    elif event.event_type == KEY_UP:
+        on_key_released(event.name)
 
 
 ### Begin ###
 
+print(f"Starting keyboard_macro {APP_VERSION}")
 read_configuration(CONFIG_FILENAME)
+keyboard.hook(lambda e: on_key_event(e))
 
 # The program continues to run, listening for the hotkey in a separate thread
 # Wait for record request
 while True:
     if record_start == True:
-        print(f"***Record [{record_name}]***")
+        print(f"***Record [{record_hot_key['name']}]***")
         print(f"Press 'Esc' to start recording.")
         keyboard.wait('esc')
         print(f"Press 'Esc' to stop.")
 
         # Record all key events until 'Esc' is pressed
-        sequences[record_position] = keyboard.record(until='esc')
-        sequences[record_position].pop()
-        sequences[record_position].pop(0)
+        record_hot_key['sequence'] = keyboard.record(until='esc')
+        record_hot_key['sequence'].pop()
+        record_hot_key['sequence'].pop(0)
 
         #set time to specified increments
         current_playback_time = 0
-        for event in sequences[record_position]:
+        for event in record_hot_key['sequence']:
             event.time = current_playback_time
             current_playback_time += key_delay
 
-        print(f"Recorded: {sequences[record_position]}")
-        save_sequence(record_position, record_filename)
+        print(f"Recorded: {record_hot_key['sequence']}")
+        save_sequence(record_hot_key)
         record_start = False
 
     time.sleep(1)
